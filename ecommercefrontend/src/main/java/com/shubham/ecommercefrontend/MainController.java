bfe030137a2bc6a4f19367eb1451e90831d45cf8
Shubham Gupta <shubham.iquincesoft@gmail.com>
2018-01-03 12:49:55 +0530

    Successfully laoded static resources and landing page

--- ecommercefrontend/src/main/java/com/shubham/ecommercefrontend/MainController.java
+++ ecommercefrontend/src/main/java/com/shubham/ecommercefrontend/MainController.java
@@ -1,7 +1,9 @@
 package com.shubham.ecommercefrontend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,7 +12,34 @@ public class MainController {
 	public ModelAndView home()
 	{
 		ModelAndView mv = new ModelAndView("index");
-		mv.addObject("grettings","Welcome to index page");
+		mv.addObject("greetings","Welcome to index page");
 		return mv;
 	}
+	
+	//difference in reqparam & pathvar
+	//by using RequestParam
+	/*@RequestMapping("/test")
+	public ModelAndView test(@RequestParam(value= "greeting", required=false)String greets)
+	{
+		if(greets==null)
+		{
+			greets="Hii";
+		}
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("greetings",greeting);
+		return mv;
+	}*/
+	
+	//By PathVariable
+	/*@RequestMapping("/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greets)
+	{
+		if(greets==null)
+		{
+			greets = "Hii";
+		}
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("greetings",greets);
+		return mv;
+	}*/
 }
